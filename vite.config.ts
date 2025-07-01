3dc2bacb3b8e999f95b5916b7383313da0786356
joshdchang <joshchang04@gmail.com>
2025-07-01 03:19:58 -0400

    cloudflare 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import contentCollections from "@content-collections/remix-vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), contentCollections()],
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    contentCollections(),
+  ],
 });
