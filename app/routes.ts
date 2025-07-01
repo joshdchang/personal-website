8dbfdca9dae83297f126e4eec872fc2985c1f51f
joshdchang <joshchang04@gmail.com>
2025-07-01 03:04:01 -0400

    good

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [
-  index("routes/home.tsx"),
-  route("about", "routes/about.tsx"),
-  route("photos", "routes/photos.tsx"),
-  route("thoughts", "routes/thoughts.tsx"),
-] satisfies RouteConfig;
+export default [index("routes/home.tsx")] satisfies RouteConfig;
--- app/routes/thoughts.tsx
+++
@@ -1,5 +0,0 @@
-import { allPosts } from "content-collections";
-
-export default function Thoughts() {
-  return <div>Thoughts</div>;
-}
